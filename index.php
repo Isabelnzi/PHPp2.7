15c0425c229a7b50edc4958892ebbc0916b8e8a6
Nzingani <isabelnzingani@outlook.com>
2018-07-26 19:44:21 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exercice3</title>
+  </head>
+  <body>
+    <p>
+<?php
+$isOk = true ;
+if($isOk == false){
+echo 'c\'est pas bon !!';
+}else{
+echo 'c\'est ok !';
+}
+?>
+</p>
+</body>
